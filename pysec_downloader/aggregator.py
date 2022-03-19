a7e92e0248d11d9237a515fe1f6401ac53f47d7c
Camelket <34602676+Camelket@users.noreply.github.com>
2022-03-20 00:11:30 +0100

    comments

--- pysec_downloader/aggregator.py
+++ pysec_downloader/aggregator.py
@@ -29,7 +29,54 @@ look for us-gaap elements that represent atms, warrants, notes ect.
 - issuance of common stock [value, from, to] us-gaap:ProceedsFromIssuanceOfCommonStock
  CommonStockSharesIssued
 - 
+
+Unit key I want is USD so a fact i want is accessed by using json["facts"][taxonomy][fact_name][unit]
+for issuance of inital public offering take the filing date as the instant of the value
+for issuance of 
 '''
+from parser.xbrl_structure import Instant, Period
+class Fact:
+    def __init__(self):
+        self.taxonomy = None
+        self.name = None
+        self.value = None
+        self.unit = None
+        self.period = None
+        self.filing_date = None
+
+def get_fact_data(self, companyfacts, name, taxonomy, unit="USD"):
+    facts = []
+    data_points = companyfacts["facts"][taxonomy][name][unit]
+    for d in data_points:
+        f = Fact()
+        if ("start" and "end") in d.keys():
+            f.period = Period(start=d["start"], end=d["end"])
+
+'''what: a fact that contains taxonomy, name, value, period, filing_date
+--> how will i use it?
+    --> pass it to db to use in calculations
+    
+    --> what do i need to write it to db?
+        the facts attributes and that is it: construct a
+        function that inserts into the right tables based
+        on fact name!
+        --> what to consider when inserting?
+        what time interests me? start, end or the filing_date (unless it is an instance where i only have to decide between filing_date and instance time)
+    does it make sense to use a Fact class or am i better
+    served with a dictionary or do i need to save space and
+    use list of lists ?
+    perform validation of period or instance where?
+    
+
+    ---> dictionary will suffice
+    
+
+'''
+
+
+
+    return 
+
 
 dl = Downloader(r"C:\Users\Olivi\Testing\sec_scraping_testing\pysec_downloader\companyfacts", user_agent="john smith js@test.com")
 
