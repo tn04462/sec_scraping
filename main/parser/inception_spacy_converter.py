00eb4d90ac19bfeb0e2dac7a2809d00669b5f1d8
Camelket <34602676+Camelket@users.noreply.github.com>
2022-04-24 18:47:11 +0200

    interim push

--- main/parser/inception_spacy_converter.py
+++ main/parser/inception_spacy_converter.py
@@ -22,10 +22,20 @@ TOKEN_TAG = "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token"
 SENTENCE_TAG = "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence"
 
 #  mostly unused 
-docs = {"train": [], "test": [], "dev": [], "total": []}
-ids = {"train": set(), "test": set(), "dev": set(), "total": set()}
-count_all = {"train": 0, "test": 0, "dev": 0, "total": 0}
-count_valid = {"train": 0, "test": 0, "dev": 0, "total": 0}
+# docs = {"train": [], "test": [], "dev": [], "total": []}
+# ids = {"train": set(), "test": set(), "dev": set(), "total": set()}
+# count_all = {"train": 0, "test": 0, "dev": 0, "total": 0}
+# count_valid = {"train": 0, "test": 0, "dev": 0, "total": 0}
+
+'''
+Useful for:
+    currently only tested for converting Inception UIMA CAS XMI (XML 1.0)
+    having 1 custom Span and 1 custom Relations Layer 
+    to spaCy Docs. 
+
+Usage:
+    docs = convert_relation_ner_to_doc(path_to/typestysteme.xml, path_to/file.xmi, split=3, split_feature="custrom.Span")
+'''
 
 def take_closest(myList, myNumber):
     """
@@ -47,7 +57,7 @@ def take_closest(myList, myNumber):
         return before
 
 
-def get_token_split_idxs(cas: Cas, split: int= 10, feature=TOKEN_TAG):
+def get_feature_split_idxs(cas: Cas, split: int= 10, feature=TOKEN_TAG):
     '''get character bounds of n splits when we split by "feature"  while respecting
     the closest sentence end bound as a cutoff point. this approache loses features(in my case: relations) 
     that go across the boundry of the last sentence!!'''
@@ -61,7 +71,7 @@ def get_token_split_idxs(cas: Cas, split: int= 10, feature=TOKEN_TAG):
     sentence_start_keys = list(sentence_start_end_map.keys())
     split_features = cas.select(feature)
     len_split_features = len(split_features)
-    min_feature_set_size = int(len_split_features / split) 
+    min_feature_set_size = int(len_split_features / split)
     feature_count = 0
     is_past_feature_size = False
     last_sentence = None
@@ -103,24 +113,24 @@ def get_token_split_idxs(cas: Cas, split: int= 10, feature=TOKEN_TAG):
 
                  
     
-def cas_select_split(token_bound_start: int, token_bound_end: int, select_result: list):
-    '''get segment of select_result between token_bound_start and token_bound_end.
+def cas_select_split(bound_start: int, bound_end: int, select_result: list):
+    '''get segment of select_result between bound_start and bound_end.
     
     Args:
-        token_bound_start: index of starting char of the token, so far found in the
+        bound_start: index of starting char of the token, so far found in the
                             Feature: de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token as "begin"
-        token_bound_end: index of last char of the token, so far found in the
+        bound_end: index of last char of the token, so far found in the
                             Feature: de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token as "end"
         select_result: return from a Cas.select() call'''
     valid_items = []
     for feature in select_result:
-        if (feature["begin"] > token_bound_start) and (feature["end"] < token_bound_end):
+        if (feature["begin"] >= bound_start) and (feature["end"] <= bound_end):
             valid_items.append(feature)
     return valid_items
 
     
 
-def convert_relation_ner_to_doc(typesysteme_filepath, xmi_filepath):
+def convert_relation_ner_to_doc(typesysteme_filepath, xmi_filepath, split: int = 1, split_feature: str = "custom.Relation"):
     '''convert a UIMA CAS XMI (XML 1.0) Entity Relation File into a spaCy Doc.
     
     currently only supports one layer of custom Relation and Span which are labeled as 
@@ -129,12 +139,16 @@ def convert_relation_ner_to_doc(typesysteme_filepath, xmi_filepath):
     Args:
         typesysteme_filepath: path to a valid typesystem.xml
         xmi_filepath: path to a valid .xmi file
+        split: how many docs you would like at most. will try to get as close while keeping
+        the feature count of the split_feature as equal as possible. will mostikely result in
+        number of segments being lower than split.
+        split_feature: the xml tag to try and split evenly by. eg: custom.Relation 
 
     Returns:
         spaCy Doc Object 
     '''
 
-    token_split_idxs = []
+    
     # declare new extension for relational data
     Doc.set_extension("rel", default={}, force=True)
     # load the annotations with cassis
@@ -142,70 +156,76 @@ def convert_relation_ner_to_doc(typesysteme_filepath, xmi_filepath):
         typesystem = load_typesystem(f)
     with open(xmi_filepath, 'rb') as f:
         annotations = load_cas_from_xmi(f, typesystem=typesystem)
-    tokens = annotations.select(TOKEN_TAG)
-    doc = Doc(vocab=nlp.vocab, words=[t.get_covered_text() for t in tokens])
-    token_start_idx_map = {}
-    for idx in range(len(tokens)):
-        token_start_idx_map[tokens[idx]["begin"]] =  idx
-
-    
-    # convert the span entities and add to doc
-    #   here i could go layer by layer to cover other use cases with multiple custom layers
-    entities = []
-    relations = {}
-    span_starts = set()
-    span_start_token_map = {}
-    map_labels = {}
-    
-    for span in annotations.select("custom.Span"):
-        # ignore invalid/nameless annotations
-        # print(span)
-        if span["Labels"] is None:
-            continue
+    feature_split_idxs = get_feature_split_idxs(cas=annotations, split=split, feature=split_feature)
+    all_tokens = annotations.select(TOKEN_TAG)
+    docs = []
+    for segment in feature_split_idxs:
+        print(segment)
+        bound_start = segment["begin"]
+        bound_end = segment["end"]
+        tokens = cas_select_split(bound_start, bound_end, all_tokens)
+        doc = Doc(vocab=nlp.vocab, words=[t.get_covered_text() for t in tokens])
+        token_start_idx_map = {}
+        entities = []
+        relations = {}
+        span_starts = set()
+        span_start_token_map = {}
+        map_labels = {}
+        for idx in range(len(tokens)):
+            token_start_idx_map[tokens[idx]["begin"]] =  idx 
+        # convert the span entities and add to doc
+        #   here i could go layer by layer to cover other use cases with multiple custom layers
         
-        doc.char_span(span["begin"], span["end"], label=span["Labels"])
-        span_starts.add(span["begin"])
-        span_start_token_map[span["begin"]] = token_start_idx_map[span["begin"]]
+        for span in cas_select_split(bound_start, bound_end, annotations.select("custom.Span")):
+            # ignore invalid/nameless annotations
+            # print(span)
+            if span["Labels"] is None:
+                continue
+            
+            doc.char_span(span["begin"], span["end"], label=span["Labels"])
+            span_starts.add(span["begin"])
+            span_start_token_map[span["begin"]] = token_start_idx_map[span["begin"]]
 
-    doc.ents = entities
-    # print(len(span_starts))
-    # print(len(token_start_idx_map))
-    # print(len(span_start_token_map))
-    # for x1 in token_idx_map.values():
-    #     for x2 in token_idx_map.values():
-    #         relations[(x1, x2)] = {}
-    
+        doc.ents = entities
+        # print(len(span_starts))
+        # print(len(token_start_idx_map))
+        # print(len(span_start_token_map))
+        # for x1 in token_idx_map.values():
+        #     for x2 in token_idx_map.values():
+        #         relations[(x1, x2)] = {}
+        
 
-    for relation in annotations.select("custom.Relation"):
-        # print(relation)
-        label = relation["Labels"]
-        if label not in map_labels:
-            map_labels[label] = label
-            # replace with token_idx_map$
-        try:
-            start = span_start_token_map[relation["Governor"]["begin"]]
-            end = span_start_token_map[relation["Dependent"]["begin"]]
-        except KeyError as e:
-            print(f"couldnt find token from relations span when converting to docbin")
+        for relation in cas_select_split(bound_start, bound_end, annotations.select("custom.Relation")):
+            # print(relation)
+            label = relation["Labels"]
+            if label not in map_labels:
+                map_labels[label] = label
+                # replace with token_idx_map$
             try:
-                start = token_start_idx_map[relation["Governor"]["begin"]]
-                end = token_start_idx_map[relation["Dependent"]["begin"]]
+                start = span_start_token_map[relation["Governor"]["begin"]]
+                end = span_start_token_map[relation["Dependent"]["begin"]]
             except KeyError as e:
-                print(f"also failed backup plan for finding relation")
-            continue
-        if (start, end) not in relations.keys():
-            relations[(start, end)] = {}
-        if label not in relations[(start, end)]:
-            relations[(start, end)][label] = 1.0
-        # if label not in relations[(start, end)]:
-        #     relations[(start, end)][label] = 1.0
-    # for x1 in token_idx_map.values():
-    #     for x2 in token_idx_map.values():
-    #         for label in map_labels.values():
-    #             if label not in relations[(x1, x2)]:
-    #                 relations[(x1, x2)][label] = 0.0
-    doc._.rel = relations
-    return doc     
+                print(f"couldnt find token from relations span when converting to docbin")
+                try:
+                    start = token_start_idx_map[relation["Governor"]["begin"]]
+                    end = token_start_idx_map[relation["Dependent"]["begin"]]
+                except KeyError as e:
+                    print(f"also failed backup plan for finding relation")
+                continue
+            if (start, end) not in relations.keys():
+                relations[(start, end)] = {}
+            if label not in relations[(start, end)]:
+                relations[(start, end)][label] = 1.0
+            # if label not in relations[(start, end)]:
+            #     relations[(start, end)][label] = 1.0
+        # for x1 in token_idx_map.values():
+        #     for x2 in token_idx_map.values():
+        #         for label in map_labels.values():
+        #             if label not in relations[(x1, x2)]:
+        #                 relations[(x1, x2)][label] = 0.0
+        doc._.rel = relations
+        docs.append(doc)
+    return docs   
         
 def docs_to_training_file(docs: list[Doc], save_path: str):
     '''create a docBin from a list of Docs and save it to save_path'''
@@ -214,15 +234,16 @@ def docs_to_training_file(docs: list[Doc], save_path: str):
 
 
 
-# doc = convert_relation_ner_to_doc(test_typesystem, test_xmi)
-# docs_to_training_file((doc,), r"C:\Users\Olivi\Desktop\spacy_example2.spacy")
+docs = convert_relation_ner_to_doc(test_typesystem, test_xmi, split=7)
+print(len(docs))
+docs_to_training_file(docs, r"C:\Users\Olivi\Desktop\spacy_example2.spacy")
 
-with open(test_typesystem, 'rb') as f:
-        typesystem = load_typesystem(f)
-with open(test_xmi, 'rb') as f:
-    cas = load_cas_from_xmi(f, typesystem=typesystem)
-splits = get_token_split_idxs(cas, split=4)
-print(splits)
+# with open(test_typesystem, 'rb') as f:
+#         typesystem = load_typesystem(f)
+# with open(test_xmi, 'rb') as f:
+#     cas = load_cas_from_xmi(f, typesystem=typesystem)
+# splits = get_feature_split_idxs(cas, split=4)
+# print(splits)
 
 
 # npa = doc.to_array()
