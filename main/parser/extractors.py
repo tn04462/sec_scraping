c160361ff63c242486ebde0cc140378120ee5a6f
Camelket <34602676+Camelket@users.noreply.github.com>
2022-06-15 12:20:25 +0200

    interim push.

--- main/parser/extractors.py
+++ main/parser/extractors.py
@@ -9,30 +9,7 @@ from .filing_nlp import SpacyFilingTextSearch
 
 logger = logging.getLogger(__name__)
 
-def int_to_roman(input):
-    """ Convert an integer to a Roman numeral. """
 
-    if not isinstance(input, type(1)):
-        raise TypeError, "expected integer, got %s" % type(input)
-    if not 0 < input < 4000:
-        raise ValueError("Argument must be between 1 and 3999")
-    ints = (1000, 900,  500, 400, 100,  90, 50,  40, 10,  9,   5,  4,   1)
-    nums = ('M',  'CM', 'D', 'CD','C', 'XC','L','XL','X','IX','V','IV','I')
-    result = []
-    for i in range(len(ints)):
-        count = int(input / ints[i])
-        result.append(nums[i] * count)
-        input -= ints[i] * count
-    return ''.join(result).lower()
-
-def roman_list():
-    return ["(" + int_to_roman(i)+")" for i in range(50)]
-
-def alphabetic_list():
-    return ["(" + letter +")" for letter in list(str.ascii_lowercase)]
-
-def numeric_list():
-    return ["(" + number + ")" for number in range(150)]
 
 
 
